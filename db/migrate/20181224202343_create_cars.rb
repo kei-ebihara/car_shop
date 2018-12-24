64925741a8eba263283c021b3c3da2aa8f40f75a
海老原恵 <kei.ebihara.hikarituushin@gmail.com>
2018-12-25 07:20:32 +0900

    車体登録機能作成

--- db/migrate/20181224202343_create_cars.rb
+++ db/migrate/20181224202343_create_cars.rb
@@ -2,8 +2,8 @@ class CreateCars < ActiveRecord::Migration[5.2]
   def change
     create_table :cars do |t|
       t.string :name, null: false, length: 70
-      t.integer :price, null: false,
-      t.date :publish_date, null: false,
+      t.integer :price, null: false
+      t.date :publish_date, null: false
       t.text :description, null: false, length: 2000
 
       t.timestamps
