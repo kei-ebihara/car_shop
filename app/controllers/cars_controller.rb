30a5fc67a88415148d2b99661659bf74d936e302
海老原恵 <kei.ebihara.hikarituushin@gmail.com>
2019-01-06 22:24:55 +0900

    カーアプリ修正3

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -19,10 +19,34 @@ class CarsController < ApplicationController
    @car = Car.find(params[:id])
  end
 
+ def edit
+   @car = Car.find(params[:id])
+ end
+
+ def update
+   @car = Car.find(params[:id])
+   if @car.update(car_params)
+     redirect_to @car, notice: "車体を更新しました。"
+   else
+     render :edit
+   end
+ end
+
+ def destroy
+   @car = Car.find(params[:id])
+   @car.destroy
+   redirect_to cars_path, notice: "車体を削除しました。"
+ end
+
+ def index
+   @cars = Car.all
+ end
+
+
  private
 
  def car_params
-   params.reqire(:car).permit(:name, :price, :publish_date, :description)
+   params.require(:car).permit(:name, :price, :publish_date, :description, :new_image)
  end
 
 end
